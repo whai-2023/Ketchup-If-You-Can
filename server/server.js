7c584c7dde992d943e21225e183c467c6e6d9fb3
Storm Harris-Studdart <103412904+StormHS@users.noreply.github.com>
2023-05-19 09:57:08 +1200

    add route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const hbs = require('express-handlebars')
+const fs = require('node:fs/promises')
+const router = express.Router()
 
 const server = express()
 
@@ -14,5 +16,17 @@ server.set('view engine', 'hbs')
 server.set('views', __dirname + '/views')
 
 // Your routes/router(s) should go here
+
+server.get('/', (req, res) => {
+  const dataPromise = fs.readFile(__dirname + '/data/data.json', 'utf8')
+  dataPromise
+    .then((json) => {
+      const data = JSON.parse(json)
+      res.render('home', data)
+    })
+    .catch((err) => {
+      res.sendStatus(500)
+    })
+})
 
 module.exports = server
